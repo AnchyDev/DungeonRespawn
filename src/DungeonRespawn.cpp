d8ceb7d5bfbefbe135c4b6d8d9746048639893c9
AnchyDev <lewienato@gmail.com>
2022-12-14 23:11:23 +1100

    Load dungeons from database and search it for start of dungeon when teleporting.

--- src/DungeonRespawn.cpp
+++ src/DungeonRespawn.cpp
@@ -38,16 +38,50 @@ void DSPlayerScript::OnPlayerReleasedGhost(Player* player)
 
 bool DSPlayerScript::OnBeforeTeleport(Player* player, uint32 /*mapid*/, float /*x*/, float /*y*/, float /*z*/, float /*orientation*/, uint32 /*options*/, Unit* /*target*/)
 {
-    if (std::count(playersToTeleport.begin(), playersToTeleport.end(), player->GetGUID()))
+    auto elementIndex = std::find(playersToTeleport.begin(), playersToTeleport.end(), player->GetGUID());
+    if (elementIndex != playersToTeleport.end())
     {
         LOG_INFO("module", "Test2");
         player->ResurrectPlayer(1.0, false);
+        playersToTeleport.erase(elementIndex);
+
+        auto dungeonIndex = std::find(dungeons.begin(), dungeons.end(), player->GetMapId());
+        if (dungeonIndex != dungeons.end())
+        {
+            auto dungeonData = dungeons.at(std::distance(dungeons.begin(), dungeonIndex));
+            player->TeleportTo(dungeonData.map, dungeonData.x, dungeonData.y, dungeonData.z, dungeonData.o);
+        }
         return false;
     }
     return true;
 }
 
+void DSWorldScript::OnAfterConfigLoad(bool /*reload*/)
+{
+    QueryResult result = WorldDatabase.Query("SELECT dungeonId, position_x, position_y, position_z, orientation FROM lfg_dungeon_template");
+
+    if (!result)
+    {
+        return;
+    }
+
+    do
+    {
+        Field* fields = result->Fetch();
+        DungeonData dungeon;
+
+        dungeon.map = fields[0].Get<uint32>();
+        dungeon.x = fields[1].Get<float>();
+        dungeon.y = fields[2].Get<float>();
+        dungeon.z = fields[3].Get<float>();
+        dungeon.o = fields[4].Get<float>();
+
+        dungeons.push_back(dungeon);
+    } while (result->NextRow());
+}
+
 void SC_AddDungeonRespawnScripts()
 {
+    new DSWorldScript();
     new DSPlayerScript();
 }
--- src/DungeonRespawn.h
+++ src/DungeonRespawn.h
@@ -2,10 +2,22 @@
 #define MODULE_DUNGEONRESPAWN_H
 
 #include "ScriptMgr.h"
+#include "LFGMgr.h"
 #include "Player.h"
 #include "Config.h"
 #include "Chat.h"
 #include <vector>
+
+struct DungeonData
+{
+    uint32 map;
+    float x;
+    float y;
+    float z;
+    float o;
+};
+
+std::vector<DungeonData> dungeons;
 
 class DSPlayerScript : public PlayerScript
 {
@@ -16,6 +28,15 @@ private:
     std::vector<ObjectGuid> playersToTeleport;
     void OnPlayerReleasedGhost(Player* /*player*/) override;
     bool OnBeforeTeleport(Player* /*player*/, uint32 /*mapid*/, float /*x*/, float /*y*/, float /*z*/, float /*orientation*/, uint32 /*options*/, Unit* /*target*/) override;
+};
+
+class DSWorldScript : public WorldScript
+{
+public:
+    DSWorldScript() : WorldScript("DSWorldScript") { }
+
+private:
+    void OnAfterConfigLoad(bool /*reload*/) override;
 };
 
 #endif //MODULE_DUNGEONRESPAWN_H
