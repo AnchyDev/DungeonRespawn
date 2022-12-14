568835c6110717a4924444514975a70a1f413b79
AnchyDev <lewienato@gmail.com>
2022-12-14 22:11:05 +1100

    Added debug messages.

--- src/DungeonRespawn.cpp
+++ src/DungeonRespawn.cpp
@@ -2,29 +2,30 @@
 
 void DSUnitScript::OnUnitDeath(Unit* unit, Unit* /*killer*/)
 {
+    LOG_INFO("module", "1");
     if (!unit)
     {
         return;
     }
-
+    LOG_INFO("module", "2");
     Player* player = unit->ToPlayer();
     if (!player)
     {
         return;
     }
 
-
+    LOG_INFO("module", "3");
     Map* map = player->GetMap();
     if (!map)
     {
         return;
     }
-
+    LOG_INFO("module", "4");
     if (!map->IsDungeon() && !map->IsRaid())
     {
         return;
     }
-
+    LOG_INFO("module", "5");
     /*LFGDungeonEntry const* dungeon = GetLFGDungeon(map->GetId(), map->GetDifficulty());
 
     if (!dungeon)
